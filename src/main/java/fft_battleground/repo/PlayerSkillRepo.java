5287831c57c5ba138def3987c78e6f0f2c80df3b
Other Brand <larryw.schultz@gmail.com>
2020-06-03 22:08:45 -0500

    added code to batch delete records for skills.

--- src/main/java/fft_battleground/repo/PlayerSkillRepo.java
+++ src/main/java/fft_battleground/repo/PlayerSkillRepo.java
@@ -3,13 +3,27 @@ package fft_battleground.repo;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import fft_battleground.repo.model.PlayerSkills;
 
+@Transactional
 public interface PlayerSkillRepo extends JpaRepository<PlayerSkills, Long> {
 
 	@Query("SELECT playerSkills FROM PlayerSkills playerSkills LEFT JOIN playerSkills.player_record playerRecord where playerRecord.player = :player ")
 	public List<PlayerSkills> getSkillsByPlayer(@Param("player") String player);
+	
+	@Modifying
+	@Query(" DELETE FROM PlayerSkills playerSkills "
+		 + " WHERE playerSkills IN "
+		 + " ("
+		 + " SELECT playerSkills "
+		 + " FROM PlayerSkills playerSkills LEFT JOIN playerSkills.player_record playerRecord "
+		 + " WHERE playerRecord.player = :player"
+		 + " ) ")
+	public void deleteSkillsByPlayer(@Param("player") String player);
+	
 }
