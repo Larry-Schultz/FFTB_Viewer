08b83fef709441f18d37dea6d706044b599c4e2e
Other Brand <larryw.schultz@gmail.com>
2020-06-16 19:29:27 -0500

    MusicEvent now has an EventType attached to it.

--- src/main/java/fft_battleground/event/model/MusicEvent.java
+++ src/main/java/fft_battleground/event/model/MusicEvent.java
@@ -5,20 +5,21 @@ import lombok.Data;
 
 @Data
 public class MusicEvent  extends BattleGroundEvent {
-	public BattleGroundEventType type = BattleGroundEventType.MUSIC;
+	private static final BattleGroundEventType EVENT_TYPE = BattleGroundEventType.MUSIC;
 	
 	private String songName;
 	private Integer durationInSeconds;
 	private String id;
 	
 	public MusicEvent(String name, Integer durationInSeconds) {
+		super(EVENT_TYPE);
 		this.songName = name;
 		this.durationInSeconds = durationInSeconds;
 	}
 
 	@Override
 	public String toString() {
-		return "MusicEvent [type=" + type + ", songName=" + songName + ", durationInSeconds=" + durationInSeconds
+		return "MusicEvent [songName=" + songName + ", durationInSeconds=" + durationInSeconds
 				+ ", id=" + id + "]";
 	}
 }
