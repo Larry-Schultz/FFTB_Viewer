bf7381ae973765ab03d0c77f3566d73f10e9be98
Other Brand <larryw.schultz@gmail.com>
2020-12-22 17:20:56 -0600

    Changed what exception is thrown when trying to reconnect to IRC

--- src/main/java/fft_battleground/irc/IrcReconnectManager.java
+++ src/main/java/fft_battleground/irc/IrcReconnectManager.java
@@ -8,17 +8,24 @@ import org.springframework.stereotype.Component;
 
 import com.gikk.twirk.Twirk;
 
+import fft_battleground.exception.IrcConnectionException;
 import fft_battleground.util.BattlegroundRetryState;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class IrcReconnectManager {
 
-	@Retryable( value = Throwable.class, maxAttempts = 30, backoff = @Backoff(delay = 20 * 1000, multiplier=2))
-	public void retryConnection(final Twirk twirk, final BattlegroundRetryState state) throws IOException, InterruptedException {
+	@Retryable( value = IrcConnectionException.class, maxAttempts = 30, backoff = @Backoff(delay = 20 * 1000, multiplier=2))
+	public void retryConnection(final Twirk twirk, final BattlegroundRetryState state) throws IrcConnectionException {
 		log.error("Attempting to reconnect to IRC");
-		twirk.connect();
 		state.incrementCount();
+		try {
+			twirk.connect();
+		} catch (IOException | InterruptedException e) {
+			throw new IrcConnectionException(e);
+		}
+		
 	}
 }
