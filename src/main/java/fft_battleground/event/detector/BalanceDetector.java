02bc6ece5c856eae9ea715aa53f4011cdcc1e220
Other Brand <larryw.schultz@gmail.com>
2020-12-30 00:47:16 -0600

    Updated the EventDetector inteface to make it generic

--- src/main/java/fft_battleground/event/detector/BalanceDetector.java
+++ src/main/java/fft_battleground/event/detector/BalanceDetector.java
@@ -3,6 +3,7 @@ package fft_battleground.event.detector;
 import org.apache.commons.lang3.StringUtils;
 
 import fft_battleground.botland.model.BattleGroundEventType;
+import fft_battleground.event.EventDetector;
 import fft_battleground.event.model.BalanceEvent;
 import fft_battleground.event.model.BattleGroundEvent;
 import fft_battleground.model.ChatMessage;
