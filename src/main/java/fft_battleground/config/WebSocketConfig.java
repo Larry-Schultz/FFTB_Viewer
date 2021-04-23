e2b3b690f7a33b6b04e09cba5e23d8df2ec2cd77
Other Brand <larryw.schultz@gmail.com>
2021-04-23 15:52:13 -0500

    removed CORS issue with websocket.

--- src/main/java/fft_battleground/config/WebSocketConfig.java
+++ src/main/java/fft_battleground/config/WebSocketConfig.java
@@ -1,4 +1,5 @@
 package fft_battleground.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -17,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:4200").withSockJS();
     }
 }
