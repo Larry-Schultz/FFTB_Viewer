e5693c508d8d46db184a22f724f57f30d654b011
Other Brand <larryw.schultz@gmail.com>
2021-06-24 12:38:01 -0500

    moving Portrait class outside of Images class

--- src/main/java/fft_battleground/image/Images.java
+++ src/main/java/fft_battleground/image/Images.java
@@ -25,8 +25,6 @@ import org.springframework.core.io.Resource;
 
 import fft_battleground.model.BattleGroundTeam;
 import fft_battleground.repo.model.TeamInfo;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -214,67 +212,6 @@ public class Images {
 			result = null;
 		}
 		
-		return result;
-	}
-}
-
-@Data
-@AllArgsConstructor
-class Portrait implements Comparable<Portrait> {
-	private String className;
-	private String gender;
-	private BattleGroundTeam color;
-	private String location;
-	
-	public Portrait() {}
-	
-	public Portrait(Path path) {
-		String fileName = path.getFileName().toString();
-		if(StringUtils.contains(fileName, "M_")) {
-			this.className = StringUtils.substringBefore(fileName, "M_");
-			this.gender = "Male";
-			this.location = fileName;
-			this.color = this.getColor(fileName);
-		} else if(StringUtils.contains(fileName, "M.")) {
-			this.className = StringUtils.substringBefore(fileName, "M.");
-			this.gender = "Male";
-			this.location = fileName;
-			this.color = BattleGroundTeam.NONE;
-		} else if(StringUtils.contains(fileName, "F_")) {
-			this.className = StringUtils.substringBefore(fileName, "F_");
-			this.gender = "Female";
-			this.color = this.getColor(fileName);
-		} else if(StringUtils.contains(fileName, "F.")) {
-			this.className = StringUtils.substringBefore(fileName, "F.");
-			this.gender = "Female";
-			this.color = BattleGroundTeam.NONE;
-		} else {
-			this.className = StringUtils.substringBefore(fileName, ".");
-			this.gender = "Monster";
-			this.color = BattleGroundTeam.NONE;
-		}
-		
-		this.location = fileName;
-	}
-	
-	public boolean isMonster() {
-		boolean result = StringUtils.equalsIgnoreCase(gender, "Monster");
-		return result;
-	}
-	
-	protected BattleGroundTeam getColor(String str) {
-		String substring = StringUtils.substringBetween(str, "_", ".");
-		BattleGroundTeam team = BattleGroundTeam.parse(substring);
-		if(team == null) {
-			team = BattleGroundTeam.NONE;
-		}
-		
-		return team;
-	}
-
-	@Override
-	public int compareTo(Portrait arg0) {
-		int result = this.className.compareTo(arg0.getClassName());
 		return result;
 	}
 }
